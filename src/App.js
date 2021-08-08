622a6abfa9c19e309864bab804fe5b177ff5d4be
Ariful Islam <iariful.info@gmail.com>
2021-08-08 20:30:54 +0600

    done navbar with pure css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <nav>
+        <div className="logo_part">
+          <a href="#logo">React Navbar</a>
+        </div>
+        <ul
+          className="nav_links"
+          style={{ transform: open ? 'translateX(0px)' : '' }}
         >
-          Learn React
-        </a>
-      </header>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <i onClick={() => setOpen(!open)} className="fas fa-bars icon_bar"></i>
+      </nav>
     </div>
   );
 }
